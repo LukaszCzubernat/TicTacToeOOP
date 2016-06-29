915d8a6814582a25b367e17ac9166de3727844e3
lukasz_czubernat <lukasz_czubernat@epam.com>
2016-06-29 09:46:01 +0200

    I had red, I had green and this is REFACTOR, so I get rid of skeleton implementation and hardcodes. Nice and clean implemetation.

--- src/main/java/model/board/Board.java
+++ src/main/java/model/board/Board.java
@@ -2,15 +2,23 @@ package model.board;
 
 import model.field.Field;
 
+import java.util.Objects;
+
 /**
  * Created by Hawk on 28.06.16.
  */
 public class Board {
-    public Board(Field[] fields) {
+    private Field[] fields;
 
+    public Board(Field[] fields) {
+        this.fields = fields;
     }
 
     public Boolean placeSign(Field field) {
-        return Boolean.TRUE;
+        if(Objects.isNull(fields[field.showIndex()])){
+            fields[field.showIndex()] = field;
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 }
--- src/main/java/model/field/Field.java
+++ src/main/java/model/field/Field.java
@@ -4,8 +4,15 @@ package model.field;
  * Created by lucz on 28.06.16.
  */
 public class Field {
-
+    private Integer index;
+    private Sign sign;
 
     public Field(Integer index, Sign sign) {
+        this.index = index;
+        this.sign = sign;
+    }
+
+    public Integer showIndex() {
+        return index;
     }
 }
